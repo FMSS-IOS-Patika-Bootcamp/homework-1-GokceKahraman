568af7a3146258dff4afe29fce0ad915e96e512c
GokceKahraman <gokcekahraman0@gmail.com>
2022-09-09 21:13:50 +0300

    Initial Commit

---
+++ homework1_Gokce_Kahraman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework1_Gokce_Kahraman
+//
+//  Created by Gökçe Kahraman on 9.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ homework1_Gokce_KahramanUITests/homework1_Gokce_KahramanUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  homework1_Gokce_KahramanUITestsLaunchTests.swift
+//  homework1_Gokce_KahramanUITests
+//
+//  Created by Gökçe Kahraman on 9.09.2022.
+//
+
+import XCTest
+
+class homework1_Gokce_KahramanUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
